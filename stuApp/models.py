4d66038eca2ab0c6e46f9c37e5037bf601c7703d
bururaru <bururaru@gmail.com>
2021-02-10 13:20:58 +0900

    StuProfile 출석 관련 추가

--- stuApp/models.py
+++ stuApp/models.py
@@ -25,10 +25,15 @@ class StuProfile(models.Model):
     user_name = models.CharField(max_length=20, blank=True)
     user_pwd = models.CharField(max_length=20, blank=True)
     user_email = models.CharField(max_length=20, blank=True)
+    user_attend = models.IntegerField(default=0)
+    user_absent = models.IntegerField(default=0)
+    user_late = models.IntegerField(default=0)
+    user_outing = models.IntegerField(default=0)
 
     bio = models.TextField(max_length=500, blank=True)
     contact = models.CharField(max_length=50, blank=True)
     location = models.CharField(max_length=30, blank=True)
+    
     # profile_img= models.ImageField(null=True)
 
     def __str__(self):
@@ -38,8 +43,6 @@ class StuProfile(models.Model):
 def create_user_stuprofile(sender, instance, created, **kwargs):
     if created:
         StuProfile.objects.create(user=instance, user_name = instance.username)
-        # bio = instance.bio, contact = instance.contact, location = instance.location, profile_img = instance.profile_img
-
 
 @receiver(post_save, sender=User)
 def save_user_stuprofile(sender, instance, **kwargs):
